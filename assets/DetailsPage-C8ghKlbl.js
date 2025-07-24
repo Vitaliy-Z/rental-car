6185acefecb977b270075a4589ecb6d6ebee148b
Vitaliy-Z <63505270+Vitaliy-Z@users.noreply.github.com>
2025-07-24 21:08:05 +0000

    Deploying to gh-pages from @ Vitaliy-Z/rental-car@b2cfe992997f7b4b35ecc95afeadf3ea37c9f9de 🚀

--- assets/DetailsPage-C8ghKlbl.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BVl6PM8x.js";const s=()=>e.jsx("div",{children:"DetailsPage"});export{s as default};
---
+++ assets/DetailsPage-DjB5fKGR.js
@@ -0,0 +1,2 @@
+import{j as e}from"./vendor-gdj_X_2t.js";const s=()=>e.jsx("div",{children:"DetailsPage"});export{s as default};
+//# sourceMappingURL=DetailsPage-DjB5fKGR.js.map
