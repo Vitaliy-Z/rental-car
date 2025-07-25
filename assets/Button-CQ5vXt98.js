ecd26257439df02921b1c1d8709c333efd0db6c2
Vitaliy-Z <63505270+Vitaliy-Z@users.noreply.github.com>
2025-07-25 11:14:17 +0000

    Deploying to gh-pages from @ Vitaliy-Z/rental-car@dd59ca9296d69a7ec52f4384562042c9caef7e29 🚀

--- assets/Button-CQ5vXt98.js
+++
@@ -1,2 +0,0 @@
-import{j as r,D as a}from"./vendor-D39fZIgC.js";const e={button:{padding:"12px",textTransform:"none",background:"var(--button)",fontWeight:600,fontSize:"16px",lineHeight:1.25,color:"var(--white)",borderRadius:"12px",boxShadow:"none","&:disabled":{background:"var(--gray)",color:"var(--white)"},"&:hover":{background:"var(--button-hover)",boxShadow:"none"}}},i=({onClick:o,style:t,children:n})=>r.jsx(a,{variant:"contained",sx:{...e.button,...t},onClick:o,children:n});export{i as B};
-//# sourceMappingURL=Button-CQ5vXt98.js.map
